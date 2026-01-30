4870800b15d2a54baa1338acc8eb84261efc6576
MDL <rhino@pacodacomenda.com>
2026-01-30 16:24:27 +0100

    feat(nvim): adds lsp modules

--- src/config/nvim/lua/configs/lspconfig.lua
+++ src/config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,40 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+
+local servers = {
+  "arduino-language-server",
+  "autotools-language-server",
+  "bash-language-server",
+  "copilot",
+  "cssls",
+  "docker-language-server",
+  "emmet-ls",
+  "eslint",
+  "html",
+  "marksman",
+  "pyright",
+  "tailwindcss",
+  "vue_ls"
+}
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- to configure lsps further read :h vim.lsp.config
+vim.lsp.config('pyright', {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = "openFilesOnly",
+          useLibraryCodeForTypes = true
+        }
+      }
+    }
+})
+
+vim.lsp.config('arduino-language-server', {})
+vim.lsp.config('autotools-language-server', {})
+vim.lsp.config('bash-language-server', {})
+vim.lsp.config('docker-language-server', {}) 
+vim.lsp.config('emmet-ls', {})
+
