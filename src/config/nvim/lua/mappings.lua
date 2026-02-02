940b753490f0a87d98074f061b600c04e2d3c809
MDL <rhino@pacodacomenda.com>
2026-02-02 16:37:06 +0100

    feat(nvim): adds run python script key map (<leader>r)

--- src/config/nvim/lua/mappings.lua
+++ src/config/nvim/lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<leader>r", ":w<CR>:!python3 %<CR>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
