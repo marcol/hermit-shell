287e285918719dac87dd582fec0f075cd8757c6b
MDL <rhino@pacodacomenda.com>
2026-02-05 22:33:31 +0100

    feat(neovim): activates and configures dashboard

--- src/config/nvim/lua/chadrc.lua
+++ src/config/nvim/lua/chadrc.lua
@@ -14,7 +14,32 @@ M.base46 = {
 	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+  -- telescope = { style = "borderless"},
+-- }
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "█▀▀▀▀▀▀▀▀▀▀█",
+    "█ █▀▀▀▀▀▀▀▀█",
+    "█▄████████ █",
+    "█ █▄▄▄▄▄▄▄▄█",
+    "█▄▄▄▄▄▄▄▄▄▄█",
+    "",
+    " Powered By  & NvChad ",
+    "",
+    "",
+  },
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+  },
+}
+
 -- M.ui = {
 --       tabufline = {
 --          lazyload = false
