b002c1306a1d7c42c4bafe8c2a123e485a977f6b
MDL <rhino@pacodacomenda.com>
2026-02-07 21:08:58 +0100

    feat(neovim): adds project management and access  - <leader>fp can be used to search for projects  - history of projects can accessed on the inital dashboard

--- src/config/nvim/lua/chadrc.lua
+++ src/config/nvim/lua/chadrc.lua
@@ -32,6 +32,7 @@ M.nvdash = {
     "",
   },
   buttons = {
+    { txt = "  Recent Projects", keys = "fp", cmd = "NeovimProjectHistory"},
     { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
     { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
     { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
