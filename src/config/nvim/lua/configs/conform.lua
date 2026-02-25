4884f44257c47255e20b779154ca7c8a62f607a7
MDL <rhino@pacodacomenda.com>
2026-02-25 11:54:03 +0100

    feat(prettier): expands config of prettier

--- src/config/nvim/lua/configs/conform.lua
+++ src/config/nvim/lua/configs/conform.lua
@@ -1,17 +1,22 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "isort", "black" },
-    javascript = { "prettier", stop_after_first = true },
     css = { "prettier" },
     html = { "prettier" },
+    javascript = { "prettier", stop_after_first = true },
+    json = { "prettier" },
+    lua = { "stylua" },
+    markdown = { "prettier" },
+    python = { "isort", "black" },
+    svelte = { "prettier" },
+    typescript = { "prettier" },
+    yaml = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  }
 }
 
 return options
